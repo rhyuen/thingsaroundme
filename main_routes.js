64be2a7c62fcba29adb79a218bc6b8edb4c79503
Robert Yuen <dungeonry@hotmail.com>
2017-07-09 19:28:58 -0700

    some local changes.

--- main_routes.js
+++ main_routes.js
@@ -1,10 +1,17 @@
 "use strict";
 
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).send("hello, thingsaroundme");
+    res.status(200).sendFile(path.join(__dirname, "views/index.html"));
 });
+
+router.get("/list", (req, res) => {
+    res.status(200).sendfile(path.join(__dirname, "views/list.html"));
+});
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 // const config = require("./config.js");
 const main_routes = require("./main_routes.js");
 const api_routes = require("./api_routes.js");
 
 let app = express();
 app.set("PORT", process.env.PORT || 1990);
+app.use(express.static(path.join(__dirname, "public")));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));

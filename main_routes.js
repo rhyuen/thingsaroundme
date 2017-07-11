7d78e8536c81047d8a8040602520ab207e1318b7
Robert Yuen <dungeonry@hotmail.com>
2017-07-11 02:24:28 -0700

    Css changes for list.html

--- main_routes.js
+++ main_routes.js
@@ -5,11 +5,11 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, "views/index.html"));
+    res.status(200).sendFile(path.join(__dirname, "public/views/index.html"));
 });
 
 router.get("/list", (req, res) => {
-    res.status(200).sendfile(path.join(__dirname, "views/list.html"));
+    res.status(200).sendfile(path.join(__dirname, "public/views/list.html"));
 });
 
 
